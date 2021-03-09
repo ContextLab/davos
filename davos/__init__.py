76f4195b8f5ede60e45a14845f697b17f00c74a9
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2021-03-09 07:01:38 -0500

    logic for dynamically selecting smuggler function

--- davos/__init__.py
+++ davos/__init__.py
@@ -1,11 +1,32 @@
+from davos import config
 
+
+def determine_environment():
+    try:
+        # noinspection PyUnresolvedReferences
+        config.IPYTHON_SHELL = get_ipython()
+    except NameError:
+        return 'PY'
+    else:
+        import IPython
+        if IPython.version_info[0] < 7:
+            # running in Colaboratory or an old IPython/Jupyter
+            # Notebook version
+            return 'IPY_OLD'
+        else:
+            # running in a new(-ish) IPython/Jupyter Notebook version
+            return 'IPY_NEW'
 
 
 version_info = (0, 0, 1)
 __version__ = '.'.join(map(str, version_info))
 
+config.PARSER_ENVIRONMENT = determine_environment()
 
 
-from typing import TypeVar, Any
-
-_T = TypeVar('_T', None, Any)
+if config.PARSER_ENVIRONMENT == 'IPY_OLD':
+    from davos.colab import smuggle_colab as smuggle
+elif config.PARSER_ENVIRONMENT == 'IPY_NEW':
+    from davos.jupyter import smuggle_jupyter as smuggle
+else:
+    from davos.python import smuggle_python as smuggle

4a90ae3f755806fefd1575a3945a5ab3d7b4b3a2
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2021-03-13 07:01:34 -0500

    prompt_input function

--- davos/core.py
+++ davos/core.py
@@ -2,16 +2,64 @@
 # once other 2 smugglers are written
 
 
-class nullcontext:
-    """
-    dummy context manager equivalent to contextlib.nullcontext
-    (which isn't implemented for Python<3.7)
-    """
-    def __init__(self, enter_result=None):
-        self.enter_result = enter_result
 
-    def __enter__(self):
-        return self.enter_result
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        return None
+
+def prompt_input(prompt, default=None, interrupt=None):
+    # ADD DOCSTRING
+    # NOTE: interrupt applies only to shell interface, not Jupyter/Colab
+    response_values = {
+        'yes': True,
+        'y': True,
+        'no': False,
+        'n': False
+    }
+    if interrupt is not None:
+        interrupt = interrupt.lower()
+        if interrupt not in response_values.keys():
+            raise ValueError(
+                f"'interrupt' must be one of {tuple(response_values.keys())}"
+            )
+    if default is not None:
+        default = default.lower()
+        try:
+            default_value = response_values[default]
+        except KeyError as e:
+            raise ValueError(
+                f"'default' must be one of: {tuple(response_values.keys())}"
+            ) from e
+        response_values[''] = default_value
+        opts = '[Y/n]' if default_value else '[y/N]'
+    else:
+        opts = '[y/n]'
+
+    while True:
+        try:
+            response = input(f"{prompt}\n{opts} ").lower()
+            return response_values[response]
+        except KeyboardInterrupt:
+            if interrupt is not None:
+                return response_values[interrupt]
+            else:
+                raise
+        except KeyError:
+            pass
+
+
+
+
+
+
+# class nullcontext:
+#     """
+#     dummy context manager equivalent to contextlib.nullcontext
+#     (which isn't implemented for Python<3.7)
+#     """
+#     def __init__(self, enter_result=None):
+#         self.enter_result = enter_result
+#
+#     def __enter__(self):
+#         return self.enter_result
+#
+#     def __exit__(self, exc_type, exc_value, traceback):
+#         return None

b44a4a0ab97f9f6193005caa4f4f359bc675e2c1
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-03-18 06:56:05 -0400

    add full plain python implementation and dummy jupyter function as placeholder

--- davos/jupyter.py
+++ davos/jupyter.py
@@ -1,8 +1,19 @@
-def smuggle_jupyter():
-    raise NotImplementedError("davos does not currently support Jupyter notebooks")
+__all__ = [
+    'register_smuggler_jupyter',
+    'run_shell_command_jupyter',
+    'smuggle_jupyter'
+]
 
 
 def register_smuggler_jupyter():
+    raise NotImplementedError("davos does not currently support Jupyter notebooks")
+
+
+def run_shell_command_jupyter(davos_, command, live_stdout=None):
+    raise NotImplementedError("davos does not currently support Jupyter notebooks")
+
+
+def smuggle_jupyter():
     raise NotImplementedError("davos does not currently support Jupyter notebooks")
 
 
--- davos/python.py
+++ davos/python.py
@@ -1,8 +1,45 @@
-def smuggle_python():
-    raise NotImplementedError("davos does not currently support pure Python interpreters")
+__all__ = [
+    'register_smuggler_python',
+    'run_shell_command_python',
+    'smuggle_python'
+]
+
+import locale
+import shlex
+import signal
+import sys
+from subprocess import CalledProcessError, PIPE, Popen
+
+from davos import davos
 
 
 def register_smuggler_python():
+    raise NotImplementedError("davos does not currently support pure Python interpreters")
+
+
+def run_shell_command_python(command):
+    cmd = shlex.split(command)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE,
+                    encoding=locale.getpreferredencoding())
+    try:
+        while True:
+            retcode = process.poll()
+            if retcode is None:
+                output = process.stdout.readline()
+                if output:
+                    sys.stdout.write(output)
+            elif retcode != 0:
+                # processed returned with non-zero exit status
+                raise CalledProcessError(returncode=retcode, cmd=cmd)
+            else:
+                return retcode
+    except KeyboardInterrupt:
+        # forward CTRL + C to process before raising
+        process.send_signal(signal.SIGINT)
+        raise
+
+
+def smuggle_python():
     raise NotImplementedError("davos does not currently support pure Python interpreters")
 
 

f605bb41b1a41a6189a19e353c2e1ef709f38e1e
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-04-10 14:55:10 -0400

    added notes for later fixes/improvements

--- davos/__init__.py
+++ davos/__init__.py
@@ -9,3 +9,15 @@ __version__ = get_distribution('davos').version
 davos = Davos()
 davos.initialize()
 smuggle = davos.smuggler
+
+
+# FIXME: find NotImplementedErrors raised during parsing step and change
+#  to ParserNotImplementedErrors
+# TODO: add tree diagram to davos.exceptions module to show class
+#  hierarchy
+# TODO: Come up with way to supply proper filename, lineno, columnno,
+#  etc. to SyntaxErrors and subclasses
+# TODO: provide a way to indicate that imported modules should ALSO be
+#  parsed by smuggle parser
+# TODO: conditionally add arguments to parsers based on user's installed
+#  version of installer

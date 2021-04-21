61f2dca11b24cc3ac5fd16717a0e59aeae1d03b0
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-04-20 23:23:20 -0400

    updated _davos stub, remove unused snippet

--- davos/_davos.py
+++ davos/_davos.py
@@ -5,7 +5,7 @@ configurable options for both internal and user settings
 """
 
 
-__all__ = ['Davos', 'capture_stdout']
+__all__ = ['capture_stdout', 'Davos']
 
 
 import sys
@@ -23,11 +23,6 @@ class Davos:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
         return cls.__instance
-
-    def confirm_installed_versions(self):
-        # run after smuggling all desired packages to confirm all
-        # *final* installed versions match requested versions
-        ...
 
     def initialize(self):
         self.confirm_install = False
--- davos/_davos.pyi
+++ davos/_davos.pyi
@@ -1,33 +1,61 @@
-from collections.abc import Callable
-from types import FunctionType
-from typing import (Any, ClassVar, Literal, Optional, Protocol, TypedDict,
-                    TypeVar)
-from ipykernel.zmqshell import ZMQInteractiveShell
+import sys
+from collections.abc import Mapping
+from io import StringIO
+from types import TracebackType
+from typing import (
+    AnyStr,
+    Callable,
+    ClassVar,
+    Generic,
+    Literal,
+    Optional,
+    overload,
+    TextIO,
+    Tuple,
+    Type,
+    TypeVar,
+    Union
+)
+from IPython.core.interactiveshell import InteractiveShell
 
-__all__: list[Literal['Davos']]
+__all__: list[Literal['capture_stdout', 'Davos']]
 
-_IPyShell = TypeVar('_IPyShell', bound=ZMQInteractiveShell)
-_Davos = TypeVar('_Davos', bound='Davos')
-
-class _SmuggleFuncProtocol(Protocol):
-    def __call__(self, name: str, as_: Optional[str] = None, **kwargs: Any) -> None: ...
-
-class SmuggleFunc(_SmuggleFuncProtocol, FunctionType):
-    _register: Callable[..., None]
-
-class LocalPkgInfo(TypedDict):
-    version: str
-    build: Optional[str]
+_D = TypeVar('_D', bound='Davos')
+_IPY_SHELL = TypeVar('_IPY_SHELL', bound=InteractiveShell)
+_StringOrFileIO = Union[StringIO, TextIO]
+_S = TypeVar('_S')
+_S1 = Tuple[_StringOrFileIO]
+_SN = Tuple[_StringOrFileIO, ...]
+_E = TypeVar('_E', bound=BaseException)
 
 class Davos:
-    __instance: ClassVar[Optional[_Davos]]
+    __instance: ClassVar[Optional[_D]]
+    _ipython_showsyntaxerror_orig: Callable[[_IPY_SHELL, Optional[str]], None]
     _shell_cmd_helper: Callable[[str], int]
+    activate_parser: Callable[[], None]
     confirm_install: bool
-    ipython_shell: Optional[_IPyShell]
+    deactivate_parser: Callable[[], None]
+    ipython_shell: _IPY_SHELL
     parser_environment: Literal['IPY_NEW', 'IPY_OLD', 'PY']
-    smuggled: dict[str, Optional[str]]
-    smuggler: SmuggleFunc
+    parser_is_active: Callable[[], bool]
+    smuggled: dict[str, str]
+    smuggler: Callable[[str, Optional[str], Literal['pip'], str, Optional[Mapping]], None]
     suppress_stdout: bool
-    def __new__(cls) -> _Davos: ...
+    def __new__(cls: Type[Davos]) -> _D: ...
     def initialize(self) -> None: ...
-    def run_shell_command(self, command: str, live_stdout: Optional[bool] = ...) -> tuple[str, int]: ...
+    def run_shell_command(self, command: str, live_stdout: Optional[bool] = None) -> tuple[str, int]: ...
+
+class capture_stdout(Generic[_S]):
+    closing: bool
+    streams: _S
+    sys_stdout_write: sys.stdout.write
+    def __init__(self, *streams: _StringOrFileIO, closing: bool = True) -> None: ...
+    @overload
+    def __enter__(self: capture_stdout[_S1]) -> _StringOrFileIO: ...
+    @overload
+    def __enter__(self: capture_stdout[_SN]) -> _SN: ...
+    @overload
+    def __exit__(self, exc_type: None, exc_value: None, traceback: None) -> None: ...
+    @overload
+    def __exit__(self, exc_type: Type[_E], exc_value: _E, traceback: TracebackType) -> bool: ...
+    def _write(self, data: AnyStr) -> None: ...

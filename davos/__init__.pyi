adebf324907c9d24442f4165b1e5f190910dff71
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-04-20 23:22:35 -0400

    updated __init__ stub

--- davos/__init__.pyi
+++ davos/__init__.pyi
@@ -1,8 +1,12 @@
-from typing import Literal
-from davos._davos import Davos, SmuggleFunc
+from collections.abc import Mapping
+from typing import Callable, Literal, Optional
+from davos._davos import Davos
 
-__all__: list[Literal['davos', 'smuggle']]
+__all__: list[Literal['activate', 'davos', 'deactivate', 'is_active', 'smuggle']]
 __version__: str
 
 davos: Davos
-smuggle: SmuggleFunc
+smuggle: Callable[[str, Optional[str], Literal['pip'], str, Optional[Mapping]], None]
+activate: Callable[[], None]
+deactivate: Callable[[], None]
+is_active: Callable[[], bool]

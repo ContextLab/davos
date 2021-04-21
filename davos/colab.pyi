431e3553c6e1de2a25544da077e5ec8f313a0055
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-04-20 23:23:39 -0400

    update colab stub

--- davos/colab.pyi
+++ davos/colab.pyi
@@ -1,13 +1,27 @@
-from typing import Optional
-from davos._davos import SmuggleFunc
+from typing import Literal, Optional
+from google.colab._shell import Shell as ColabShell
 
-__all__: list[str]
+__all__: list[
+    Literal[
+        'activate_parser_colab',
+        'check_parser_active_colab',
+        'deactivate_parser_colab',
+        'run_shell_command_colab',
+        'smuggle_colab',
+        'smuggle_parser_colab'
+    ]
+]
 
-def register_parsers_colab() -> None: ...
-
-def run_shell_command_colab(command: str) -> int: ...
-
-smuggle_colab: SmuggleFunc
-# def smuggle_colab(pkg_name: str, as_: Optional[str] = ...) -> None: ...
-
+def _showsyntaxerror_davos(colab_shell: ColabShell, filename: Optional[str] = None) -> None: ...
+def activate_parser_colab() -> None: ...
+def check_parser_active_colab() -> bool: ...
+def deactivate_parser_colab() -> None: ...
+def run_shell_command_colab(command: str) -> Literal[0]: ...
+def smuggle_colab(
+        name: str,
+        as_: Optional[str] = None,
+        installer: [Literal['pip']] = 'pip',
+        args_str: str = '',
+        installer_kwargs: Optional[dict[str, ...]] = None
+) -> None: ...
 def smuggle_parser_colab(line: str) -> str: ...

09cd6b1c865222e5899cae8a0ebedc43576ec213
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-05-12 21:59:34 -0400

    initial implementation for DavosConfig class

---
+++ davos/core/config.py
@@ -0,0 +1,135 @@
+import pathlib
+import sys
+
+from davos.core.exceptions import DavosConfigError
+
+
+class Singleton(type):
+    __instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super().__call__(*args, **kwargs)
+        return cls.__instances[cls]
+
+
+class DavosConfig(metaclass=Singleton):
+    @staticmethod
+    def _get_stdlib_modules():
+        stdlib_dir = pathlib.Path(pathlib.__file__).parent
+        stdlib_modules = set(p.stem for p in stdlib_dir.iterdir())
+        try:
+            stdlib_modules.remove('site-packages')
+        except KeyError:
+            pass
+        if sys.platform.startswith('linux'):
+            try:
+                stdlib_modules.remove('dist-packages')
+            except KeyError:
+                pass
+        return stdlib_modules
+    
+    def __init__(self):
+        ########################################
+        #           READ-ONLY FIELDS           #
+        ########################################
+        try:
+            # noinspection PyUnresolvedReferences
+            self._ipython_shell = get_ipython()
+        except NameError:
+            # imported from a non-interactive Python script
+            self._ipython_shell = None
+            self._environment = 'Python'
+        else:
+            import IPython
+            if IPython.version_info[0] < 7:
+                if 'google.colab' in str(self._ipython_shell):
+                    self._environment = 'Colaboratory'
+                else:
+                    self._environment = 'IPython<7.0'
+            else:
+                self._environment = 'IPython>=7.0'
+        self._smuggled = {}
+        self._stdlib_modules = self._get_stdlib_modules()
+        ########################################
+        #          CONFIGURABLE FIELDS         #
+        ########################################
+        self._active = True
+        self._allow_rerun = False
+        self._confirm_install = False
+        self._suppress_stdout = False
+
+    def __repr__(self):
+        # TODO: implement me 
+        #  also implement _repr_html_ and/or _repr_pretty_ et al.
+        return super().__repr__()
+        
+    @property
+    def active(self):
+        return self._active
+    
+    @active.setter
+    def active(self, state):
+        if state is True:
+            from davos.implementations import activate as switch
+        elif state is False:
+            from davos.implementations import deactivate as switch
+        else:
+            raise DavosConfigError('active', "field may be 'True' or 'False'")
+        switch()
+        
+    @property
+    def allow_rerun(self):
+        return self._allow_rerun
+    
+    @allow_rerun.setter
+    def allow_rerun(self, value):
+        if not isinstance(value, bool):
+            raise DavosConfigError('allow_rerun', 
+                                   "field may be 'True' or 'False'")
+        self._allow_rerun = value
+        
+    @property
+    def confirm_install(self):
+        return self._confirm_install
+    
+    @confirm_install.setter
+    def confirm_install(self, value):
+        if not isinstance(value, bool):
+            raise DavosConfigError('confirm_install', 
+                                   "field may be 'True' or 'False'")
+        self._confirm_install = value
+        
+    @property
+    def environment(self):
+        return self._environment
+    
+    @environment.setter
+    def environment(self, _):
+        raise DavosConfigError('environment', 'field is read-only')
+        
+    @property
+    def ipython_shell(self):
+        return self._ipython_shell
+    
+    @ipython_shell.setter
+    def ipython_shell(self, _):
+        raise DavosConfigError('ipython_shell', 'field is read-only')
+
+    @property
+    def smuggled(self):
+        return self._smuggled
+    
+    @smuggled.setter
+    def smuggled(self, _):
+        raise DavosConfigError('smuggled', 'field is read-only')
+        
+    @property
+    def suppress_stdout(self):
+        return self._suppress_stdout
+    
+    @suppress_stdout.setter
+    def suppress_stdout(self, value):
+        if not isinstance(value, bool):
+            raise DavosConfigError('suppress_stdout', 
+                                   "field may be 'True' or 'False'")
+        self._suppress_stdout = value

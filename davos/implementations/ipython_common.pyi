f7011e18d9c7b7aa0fd71b1272b2495208aba5b7
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-05-18 10:23:09 -0400

    WIP stub file for ipython_common

---
+++ davos/implementations/ipython_common.pyi
@@ -0,0 +1,23 @@
+from typing import Literal, Optional, TypeVar
+from ipykernel.zmqshell import ZMQInteractiveShell
+from davos.core.core import PipInstallerKwargs
+
+__all__ = list[Literal['check_conda', 'smuggle']]
+
+_IpyShell = TypeVar('_IpyShell', bound=ZMQInteractiveShell)
+
+def _run_shell_command_helper(command: str) -> None: ...
+def _set_custom_showsyntaxerror() -> None: ...
+def _showsyntaxerror_davos(
+        ipy_shell: _IpyShell, 
+        filename: Optional[str] = ..., 
+        running_compiled_code: bool = ...
+) -> None: ...
+def check_conda() -> None: ...
+def smuggle(
+        name: str, 
+        as_: Optional[str] = ..., 
+        installer: [Literal['conda', 'pip']] = ..., 
+        args_str: str = ..., 
+        installer_kwargs: Optional[PipInstallerKwargs] = ...
+) -> None: ...

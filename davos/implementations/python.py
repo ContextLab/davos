4d0ce9c7d489f8651ff4c4a5d98874ed1fecca88
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-05-18 20:19:12 -0400

    add _run_shell_command_helper for pure python

--- davos/implementations/python.py
+++ davos/implementations/python.py
@@ -4,7 +4,35 @@
 # TODO: add __all__
 
 
+import locale
+import shlex
+import signal
+import sys
+from subprocess import CalledProcessError, PIPE, Popen
+
+
 def _activate_helper(smuggle_func, parser_func): ...
 
 
 def _deactivate_helper(smuggle_func, parser_func): ...
+
+
+def _run_shell_command_helper(command):
+    # ADD DOCSTRING
+    cmd = shlex.split(command)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE,
+                    encoding=locale.getpreferredencoding())
+    try:
+        while True:
+            retcode = process.poll()
+            if retcode is None:
+                output = process.stdout.readline()
+                if output:
+                    sys.stdout.write(output)
+            elif retcode != 0:
+                # processed returned with non-zero exit status
+                raise CalledProcessError(returncode=retcode, cmd=cmd)
+    except KeyboardInterrupt:
+        # forward CTRL + C to process before raising
+        process.send_signal(signal.SIGINT)
+        raise

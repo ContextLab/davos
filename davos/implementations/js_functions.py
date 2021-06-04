a9a4279b9c2e4c4f945194de2394477b9d1ed414
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-06-04 16:49:42 -0400

    finished displayButtonPrompt func

--- davos/implementations/js_functions.py
+++ davos/implementations/js_functions.py
@@ -46,21 +46,34 @@ JS_FUNCTIONS = DotDict({
         'displayButtonPrompt': """\
             /**
              * Display one or more buttons for user selection and block until one is clicked
-             * @param {...Object} buttonArgs - Objects containing data for each button/option 
-             *     to be displayed.
-             * @param {string} [buttonArgs.text="Button X"] - Text label for the given button. 
-             *     Defaults to "Button X", where "X" is the button number (index + 1).
-             * @param {function} [buttonArgs.onClick] - Callback run when the given button is 
-             *     clicked. Defaults to `console.log('Button X clicked');`, where X has the 
-             *     same meaning as above.
-             * @param {string} [buttonArgs.id] - Optional id for the given button element.
-             * @param {*} [buttonArgs.result] - 
+             * @param {Object[]} buttonArgs - Array of objects containing data for each 
+             *     button/option to be displayed.
+             * @param {String} [buttonArgs[].text=""] - Text label for the given button. 
+             *     Defaults to an empty string.
+             * @param {(BigInt|Boolean|null|Number|Object|String|undefined)} [buttonArgs[].result] - 
+             *     Value assigned to the "result" trait of the ipywidgets object whose 
+             *     model_id attribute is widgetModelId (if provided), if the given button is 
+             *     clicked. Used to forward user input information to Python. JS types are 
+             *     converted ty Python types, within reason (Boolean -> bool, Object -> dict, 
+             *     null/undefined -> None, etc.). If omitted, the return value of onClick 
+             *     will be used instead.
+             * @param {Function} [buttonArgs[].onClick] - Callback run when the given button is 
+             *     clicked, before the result value is set on the ipywidgets object and 
+             *     Python execution resumes. Omit the result property to use the return value 
+             *     of this function as a dynamically computed result value. 
+             *     Defaults to a noop.
+             * @param {String} [buttonArgs[].id] - Optional id for the given button element.
+             * @param {String} [widgetModelId] - The ID of the ipywidgets WidgetModel object 
+             *     whose "result" property will be assigned the result value for the clicked 
+             *     button.
              */
-            const displayButtonPrompt = function(...buttonArgs) {
+            const displayButtonPrompt = async function(buttonArgs, widgetModelId) {
+                let onClick, clickedButtonCallback, clickedButtonResult, resolutionFunc;
                 const outputArea = this,
                     outputDisplayArea = element[0],
+                    // store resolve function in outer scope so it can be called from an 
+                    // event listener
                     callbackPromise = new Promise((resolve) => resolutionFunc = resolve );
-                let onClick, clickedButtonCallback, resolutionFunc
                 
                 buttonArgs.forEach(function (buttonObj, ix) {
                     let buttonElement = document.createElement('BUTTON');
@@ -70,25 +83,41 @@ JS_FUNCTIONS = DotDict({
                         buttonElement.id = buttonObj.id;
                     }
                     if (typeof buttonObj.text === 'undefined') {
-                        buttonElement.textContent = `Button ${ix + 1}`;
+                        buttonElement.textContent = '';
                     } else {
                         buttonElement.textContent = buttonObj.text;
                     }
-                    onClick = (typeof buttonObj.onClick === 'undefined') ? () => {} : buttonObj.onClick;
-                    buttonElement.addEventListener('click', () => {
-                        clickedButtonCallback = onClick;
-                        resolutionFunc();
-                    })
-                    
                     if (typeof buttonObj.onClick === 'undefined') {
-                        onClick = () => console.log(`Button ${ix + 1} clicked`);
+                        onClick = () => {};
                     } else {
-                        onClick = buttonObj.onClick
+                        onClick = buttonObj.onClick;
                     }
+                    buttonElement.addEventListener('click', () => {
+                        // store clicked button's callback & result, resolve awaited promise
+                        clickedButtonCallback = onClick;
+                        clickedButtonResult = buttonObj.result;
+                        resolutionFunc();
+                    });
+                    outputDisplayArea.appendChild(buttonElement);
+                })
+                // attach handler to run when promise is fulfilled, pause until button is clicked
+                await callbackPromise.then(() => {
+                    // TODO: make sure this doesn't remove the widget model before we need it below
+                    outputDisplayArea.remove();
+                    const CbReturnVal = clickedButtonCallback();
                     
-                    
-                });
-                
+                    if (typeof widgetModelId !== 'undefined') {
+                        if (typeof clickedButtonResult === 'undefined') {
+                            clickedButtonResult = CbReturnVal;
+                        }
+                        const widgetManager = Jupyter.WidgetManager._managers[0],
+                            widgetModel = widgetManager.get_model(widgetModelId);
+                        widgetModel.then((model) => {
+                            model.set('result', clickedButtonResult);
+                            model.save_changes();
+                        })
+                    }
+                })
             }.bind(this)
         """
     }

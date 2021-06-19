3fbc3d11c105f9798d2b1439950ac825f0e5143a
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-06-18 23:00:17 -0400

    jupyter notebook driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,24 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 
-class ColabNotebookDriver:
-    def __init__(self, url, driver_path):
-        self.driver = webdriver.Firefox(executable_path=driver_path)
+class NotebookDriver:
+    def __init__(self, url, browser='firefox'):
+        self.url = url
+        if browser.lower() != 'firefox':
+            raise NotImplementedError("Test only implemented for Firefox")
+        self.driver = webdriver.Firefox(executable_path=getenv('DRIVER_PATH'))
         self.driver.get(url)
-        self._sign_in_google()
-        self._run_all_cells()
 
-    def _sign_in_google(self):
+
+class ColabDriver(NotebookDriver):
+    def __init__(self, notebook_path, browser='firefox'):
+        username = getenv("GITHUB_ACTOR")
+        ref = getenv("GITHUB_SHA")
+        url = f"https://colab.research.google.com/github/{username}/davos/blob/{ref}/{notebook_path}"
+        super().__init__(url=url, browser=browser)
+        self.sign_in_google()
+
+    def sign_in_google(self):
         # click "Sign in" button
         self.driver.find_element_by_css_selector("#gb > div > div.gb_Se > a").click()
         # enter email
@@ -27,8 +37,8 @@ class ColabNotebookDriver:
         pwd_input_box = self.driver.find_element_by_name("password")
         pwd_input_box.send_keys(getenv("GMAIL_PASSWORD"))
         self.driver.find_element_by_id("passwordNext").click()
-
-    def _run_all_cells(self, pre_approved=False):
+        
+    def run_all_cells(self, pre_approved=False):
         keyboard_shortcut = ActionChains(self.driver) \
             .key_down(Keys.META).send_keys() \
             .send_keys(Keys.F9) \
@@ -39,3 +49,17 @@ class ColabNotebookDriver:
             # (button takes a second to become clickable)
             time.sleep(3)
             self.driver.find_element_by_id("ok").click()
+
+
+class JupyterDriver(NotebookDriver):
+    def __init__(self, notebook_path, ip='127.0.0.1', port='8888', browser='firefox'):
+        url = f"http://{ip}:{port}/notebooks/{notebook_path}"
+        super().__init__(url=url, browser=browser)
+        
+    def run_all_cells(self):
+        self.driver.find_element_by_id("run_all_cells").click()
+        
+    def set_kernel(self, kernel_name):
+        self.driver.find_element_by_id(f"kernel-submenu-{kernel_name} > a").click()
+        # allow time for kernel to change
+        time.sleep(5)

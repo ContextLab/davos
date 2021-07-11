b4a485c2ed171e18d19f4e4b42b1080e201a387d
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2021-07-11 15:19:17 -0400

    fixed full-cell parser function for IPython>=7

--- davos/implementations/ipython_post7.py
+++ davos/implementations/ipython_post7.py
@@ -4,6 +4,8 @@
 __all__ = ['generate_parser_func']
 
 
+from IPython.core.inputtransformer import assemble_python_lines
+
 from davos import config
 
 
@@ -47,13 +49,48 @@ def _deactivate_helper(smuggle_func, parser_func):
 
 def generate_parser_func(line_parser):
     # ADD DOCSTRING
-    def full_parser(lines):
-        # each line ends with '\n', so drop last character before 
-        # parsing & re-append newline afterwards
-        return [f'{line_parser(line[:-1])}\n' for line in lines]
+    pyline_assembler = assemble_python_lines()
 
-    # prevents transformer from being run multiple times when 
-    # IPython parses partial line to determine whether input is 
+    def full_parser(lines):
+        if 'smuggle ' not in ''.join(lines):
+            # if cell contains no potential smuggle statements, don't
+            # bother parsing line-by-line
+            return lines
+
+        parsed_lines = []
+        curr_buff = []
+        for ix, raw_line in enumerate(lines):
+            # don't include trailing '\n'
+            python_line = pyline_assembler.push(raw_line[:-1])
+            if python_line is None:
+                # currently accumulating multiline logical line
+                curr_buff.append(raw_line)
+                continue
+
+            # pass single-line parser full logical lines -- may be
+            # single physical line or fully accumulated multiline
+            # statement
+            parsed_line = line_parser(python_line)
+            if curr_buff:
+                # logical line contains multiple physical lines
+                if parsed_line == python_line:
+                    # if multiline statement is not a smuggle statement,
+                    # don't combine physical lines in output
+                    parsed_lines.extend(curr_buff)
+                    # last line isn't in curr_buff, so add it separately
+                    parsed_lines.append(raw_line)
+                else:
+                    # logical line is a multiline smuggle statement
+                    parsed_lines.append(f'{parsed_line}\n')
+                # reset partially accumulated lines
+                curr_buff.clear()
+            else:
+                # logical line contains single physical line
+                parsed_lines.append(f'{parsed_line}\n')
+        return parsed_lines
+
+    # prevents transformer from being run multiple times when
+    # IPython parses partial line to determine whether input is
     # complete
     full_parser.has_side_effects = True
     return full_parser

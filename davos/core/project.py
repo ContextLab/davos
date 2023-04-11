15176cb0b98cb2f9ec293798d1d5ac3fbbd4ccfa
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2023-04-11 09:23:35 -0400

    methods for dynamically morphing between AbstractProject and ConcreteProject, renaming projects

--- davos/core/project.py
+++ davos/core/project.py
@@ -39,51 +39,18 @@ class ProjectChecker(type):
     """
     def __call__(cls, name):
         """TODO: add docstring"""
-        cls_to_init = ConcreteProject
-        # if user passed a pathlib.Path, convert it to a str so it can
-        # be properly expanded, substituted, resolved, etc. below
-        name = str(name)
-        if PATHSEP in name:
-            # `name` is a path to a notebook file, either the default
-            # project (path to the current notebook) or user-specified.
-            # File doesn't *have* to exist at this point (will be an
-            # AbstractProject, if not), but must at least point to what
-            # could eventually be a notebook
-            # TODO: separate this off into some sort of "resolve path"
-            #  utility function?
-            name_path = Path(expandvars(name)).expanduser().resolve(strict=False)
-            if name_path.suffix != '.ipynb' or name_path.is_dir():
-                raise DavosProjectError(
-                    f"Invalid project name: {name!r} (which resolves to "
-                    f"{name_path!r}). Project names may be either a simple "
-                    f"name (without {PATHSEP!r}) or a path to a Jupyter "
-                    f"notebook file (ending in .ipynb)."
-                )
-            if not name_path.is_file():
-                cls_to_init = AbstractProject
-            name = str(name_path)
-        elif PATHSEP_REPLACEMENT in name:
-            # `name` is a path-like project directory name read from
-            # DAVOS_PROJECT_DIR. Convert back to normal path format to
-            # check whether it exists, but don't want to do any
-            # validation here in case user somehow ended up with
-            # malformed Project dir name, since that could cause
-            # incessant errors until manually fixed. Instead, just make
-            # it an AbstractProject and let user rename or delete it
-            # via davos
-            name_path = Path(f"{name.replace(PATHSEP_REPLACEMENT, PATHSEP)}.ipynb")
-            if not name_path.is_file():
-                cls_to_init = AbstractProject
-            name = str(name_path)
+        cleaned_name, cls_to_init = _get_project_name_type(name)
         # `name` passed to __init__ is now a str: either a simple name
         # or a fully substituted path to a .ipynb file
-        return type.__call__(cls_to_init, name)
+        return type.__call__(cls_to_init, cleaned_name)
 
 
 class Project(metaclass=ProjectChecker):
     """
     A pseudo-environment associated with a particular (set of)
     davos-enhanced notebook(s)
+    # TODO: future feature: automatic conversion between ConcreteProject
+       and AbstractProject reference notebook is moved/created/deleted
     """
     def __init__(self, name):
         """
@@ -151,10 +118,7 @@ class Project(metaclass=ProjectChecker):
             self._site_packages_mtime = site_pkgs_mtime
 
     def _set_names(self, name):
-        """
-        TODO: add docstring -- separate method so it can be called when
-         renaming project to update names
-        """
+        """set various name-related attributes given the project name"""
         self.name = name
         self.safe_name = name.replace(PATHSEP, PATHSEP_REPLACEMENT).replace('.ipynb', '')
         self.project_dir = DAVOS_PROJECT_DIR.joinpath(self.safe_name)
@@ -163,6 +127,18 @@ class Project(metaclass=ProjectChecker):
     def freeze(self):
         """pip-freeze-like output for the Project"""
         return '\n'.join('=='.join(pkg) for pkg in self.installed_packages)
+
+    def reload(self):
+        """TODO: add docstring"""
+        # NOTE: this currently busts cache of installed packages
+        template_instance = Project(self.name)
+        self.__class__ = template_instance.__class__
+        self.__dict__ = template_instance.__dict__
+        # explicitly delete the temporary new Project instance so its
+        # __del__ method is called before this method returns and we
+        # can ensure the project directory exists after reload
+        del template_instance
+        self.project_dir.mkdir(parents=False, exist_ok=True)
 
     def remove(self, yes=False):
         """
@@ -180,8 +156,49 @@ class Project(metaclass=ProjectChecker):
         shutil.rmtree(self.project_dir)
 
     def rename(self, new_name):
-        """rename the project directory, possibly due to renaming/moving notebook"""
-        raise NotImplementedError
+        """
+        rename the project and its directory accordingly, possibly
+        converting from a ConcreteProject to an AbstractProject or vice
+        versa
+        """
+        new_project_name, new_project_type = _get_project_name_type(new_name)
+        if new_project_name == self.name:
+            # no change
+            return
+        new_safe_name = new_project_name.replace(PATHSEP, PATHSEP_REPLACEMENT).replace('.ipynb', '')
+        new_project_dir = DAVOS_PROJECT_DIR.joinpath(new_safe_name)
+        if (
+                new_project_dir.is_dir() and
+                next(new_project_dir.iterdir(), None) is not None
+        ):
+            # new project dir exists and is non-empty
+            raise DavosProjectError(
+               f"a Project named {new_project_name!r} already exists and "
+               "is non-empty. To use this name for another project, first "
+               "`.remove()` the existing project."
+            )
+        # rename the project directory
+        self.project_dir.rename(new_project_dir)
+        # reload self with new name and type, but retain the installed
+        # package cache since we're just renaming the project and not
+        # modifying its contents. Note: don't really *need* to do this
+        # if `isinstance(self, new_project_type)`, but this way is less
+        # likely to introduce bugs in the future if any of the *Project
+        # classes' constructors change
+        old_installed_pkgs = self._installed_packages
+        old_site_pkgs_mtime = self._site_packages_mtime
+        # can call type.__call__ directly to bypass metaclass's __call__
+        # since we already know the Project's new type
+        template_instance = type.__call__(new_project_type, new_project_name)
+        self.__class__ = template_instance.__class__
+        self.__dict__ = template_instance.__dict__
+        self._installed_packages = old_installed_pkgs
+        self._site_packages_mtime = old_site_pkgs_mtime
+        # explicitly delete the temporary new Project instance so its
+        # __del__ method is called before this method returns and we
+        # can ensure the project directory exists after reload
+        del template_instance
+        self.project_dir.mkdir(parents=False, exist_ok=True)
 
 
 class AbstractProject(Project):
@@ -205,6 +222,46 @@ class AbstractProject(Project):
 class ConcreteProject(Project):
     """TODO: add docstring"""
 
+
+def _get_project_name_type(name):
+    """TODO: add docstring"""
+    project_type = ConcreteProject
+    # if user passed a pathlib.Path, convert it to a str so it can
+    # be properly expanded, substituted, resolved, etc. below
+    project_name = str(name)
+    if PATHSEP in project_name:
+        # `name` is a path to a notebook file, either the default
+        # project (path to the current notebook) or user-specified.
+        # File doesn't *have* to exist at this point (will be an
+        # AbstractProject, if not), but must at least point to what
+        # could eventually be a notebook
+        # TODO: carve this off into some sort of "resolve path" utility
+        #  function?
+        name_path = Path(expandvars(project_name)).expanduser().resolve(strict=False)
+        if name_path.suffix != '.ipynb' or name_path.is_dir():
+            raise DavosProjectError(
+                f"Invalid project name: {name!r} (which resolves to "
+                f"{name_path!r}). Project names may be either a simple "
+                f"name (without {PATHSEP!r}) or a path to a Jupyter "
+                f"notebook file (ending in .ipynb)."
+            )
+        if not name_path.is_file():
+            project_type = AbstractProject
+        project_name = str(name_path)
+    elif PATHSEP_REPLACEMENT in project_name:
+        # `name` is a path-like project directory name read from
+        # DAVOS_PROJECT_DIR. Convert back to normal path format to
+        # check whether it exists, but don't want to do any
+        # validation here in case user somehow ended up with
+        # malformed Project dir name, since that could cause
+        # incessant errors until manually fixed. Instead, just make
+        # it an AbstractProject and let user rename or delete it
+        # via davos
+        name_path = Path(f"{project_name.replace(PATHSEP_REPLACEMENT, PATHSEP)}.ipynb")
+        if not name_path.is_file():
+            project_type = AbstractProject
+        project_name = str(name_path)
+    return project_name, project_type
 
 
 def get_notebook_path():
@@ -263,10 +320,6 @@ def cleanup_project_dir_atexit(dirpath):
             dirpath.rmdir()
         except OSError:
             pass
-
-
-def prune_project(proj):
-    """delete a single project by its name"""
 
 
 def prune_projects():
